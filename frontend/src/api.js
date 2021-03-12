c446dae43589e5709d062e4fe131fa635c92aaf6
vrodgreg <75463166+vrodgreg@users.noreply.github.com>
2021-03-12 13:23:08 -0500

    update with correct URL for heroku

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 // const baseURL = `http://localhost:5000/api`;
-const baseURL = 'https://iron-flow.netlify.app/api'
+const baseURL = 'https://iron-flow.herokuapp.com/'
 const token = localStorage.getItem("token");
 
 const API = axios.create({
